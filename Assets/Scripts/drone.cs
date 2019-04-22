c06ba84606db52266af1d7617a02a0b3c3198bed
Christopher Dignam <chris@dignam.xyz>
2019-04-22 18:05:30 -0400

    WIP: start mesh communication

--- Assets/Scripts/drone.cs
+++ Assets/Scripts/drone.cs
@@ -1,28 +1,98 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+
+enum ConnectionStatus { Disconnected, Started, Connected, Failed };
 
 public class drone : MonoBehaviour
 {
-    List <Vector3> history = new List<Vector3>();
+    List<Vector3> history = new List<Vector3>();
     List<Vector3> waypoints = new List<Vector3>();
-    Vector3 dest;
     public float speed;
-    Vector3 pt;
+    const int MAX_COMMUNICATION_DISTANCE = 10;
+    const int COMMUNICATION_FRAME_COUNT = 20;
+    public bool connected = false;
+
+    // connection status from this Drone to all other neighbor Drones
+    Dictionary<drone, ConnectionStatus> neighbors = new Dictionary<drone, ConnectionStatus>();
 
     // Start is called before the first frame update
     void Start()
     {
-        pt = new Vector3(0,0,0);
-        SetWaypoint(pt);
+        SetWaypoint(new Vector3(0, 0, 0));
+    }
+
+    private int FrameCount = 0;
+
+    void MessageNeighbors()
+    {
+        // only run sometimes
+        if (FrameCount == COMMUNICATION_FRAME_COUNT)
+        {
+            FrameCount = 0;
+            var hitColliders = Physics.OverlapSphere(transform.position, MAX_COMMUNICATION_DISTANCE * 1000);
+            foreach (Collider c in hitColliders)
+            {
+                var maybeDrone = c.GetComponent<drone>();
+                if (maybeDrone != null)
+                {
+                    Debug.Log($"collision {c}");
+
+                    maybeDrone.SendMessage("Ping", this);
+                }
+
+            }
+        }
+        FrameCount++;
+    }
+
+    // advance connection status from sender to next state
+    void NeighborNextState(Dictionary<drone, ConnectionStatus> droneStatuses, drone remoteDrone)
+    {
+        if (neighbors.ContainsKey(remoteDrone))
+        {
+            var currentStatus = neighbors[remoteDrone];
+            switch (currentStatus)
+            {
+                case ConnectionStatus.Disconnected:
+                    neighbors[remoteDrone] = ConnectionStatus.Started;
+                    break;
+                case ConnectionStatus.Started:
+                    neighbors[remoteDrone] = ConnectionStatus.Connected;
+                    break;
+
+            }
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, waypoints[0], speed * Time.deltaTime);
+        MessageNeighbors();
+        if (waypoints.Count > 0)
+        {
+            transform.position = Vector3.Lerp(transform.position, waypoints[0], speed * Time.deltaTime);
+
+        }
         //Debug.Log(transform.position + " | " + waypoints[0]);
         //Debug.Log(waypoints.Count);
+
+
+        foreach (var v in neighbors.Values)
+        {
+            if (v == ConnectionStatus.Connected)
+            {
+                connected = true;
+                break;
+            }
+        }
+    }
+
+    // Contacted by Drone that wants to communicate
+    public void Ping(drone sender)
+    {
+        Debug.Log($"received ping from: {sender}");
+        NeighborNextState(neighbors, sender);
     }
 
     public void Delete()
@@ -33,7 +103,7 @@ public class drone : MonoBehaviour
     public void SetWaypoint(Vector3 point)
     {
         waypoints.Add(point);
-        foreach(Vector3 item in waypoints)
+        foreach (Vector3 item in waypoints)
         {
             Debug.Log(item);
         }
